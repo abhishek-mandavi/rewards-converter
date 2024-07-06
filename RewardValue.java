ce80187e0fb7a4579e0713ebff112dc369bd6084
Abhishek <141568691+abhishek-mandavi@users.noreply.github.com>
2024-07-06 16:11:31 +0530

    Add files via upload
    Added a new class named RewardValue to the main.java package. RewardValue has two constructors: one that accepts a cash value and one that accepts a value in miles.
     RewardValue had a getCashValue() method, which returns the cash value of the RewardValue.
     RewardValue had a getMilesValue() method, which returns how many miles the RewardValue is worth.
     RewardValue converted from miles to cash at a rate of 0.0035.

---
+++ RewardValue.java
@@ -0,0 +1,40 @@
+public class RewardValue {
+    private double cashValue;
+    private double milesValue;
+
+    private double miles_to_cash_rate;
+
+    public RewardValue(double cashValue) {
+        this.cashValue = cashValue;
+        this.milesValue = cashValue / 0;
+    }
+
+    public RewardValue(double milesValue, boolean isMiles) {
+        if (isMiles) {
+            this.milesValue = milesValue;
+            this.cashValue = milesValue * miles_to_cash_rate;
+        } else {
+            this.cashValue = milesValue;
+            this.milesValue = milesValue / 0;
+        }
+    }
+
+    public double getCashValue() {
+        return cashValue;
+    }
+
+    public double getMilesValue(){
+        return milesValue;
+    }
+
+    public static  void main(String[] args) {
+
+        RewardValue rewardInCash = new RewardValue(100); // cash value
+        System.out.println("Cash value: $" + rewardInCash.getCashValue());
+        System.out.println("Miles value: " + rewardInCash.getMilesValue() + " miles");
+
+        RewardValue rewardInMiles = new RewardValue(1000, true); // miles value
+        System.out.println("Cash value: $" + rewardInMiles.getCashValue());
+        System.out.println("Miles value: " + rewardInMiles.getMilesValue() + " miles");
+    }
+}
